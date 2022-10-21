4d2582e596d81b3794ca213ce3f82e19c152d599
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2022-10-21 12:45:39 +0900

    研修で使用するインストーラのメモとセットアップスクリプトを作成

---
+++ installers/install.bat
@@ -0,0 +1,10 @@
+Git-2.37.2.2-64-bit.exe /VERYSILENT /LOG
+
+VSCodeUserSetup-x64-1.70.0.exe /VERYSILENT /NORESTART /LOG /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension eamodio.gitlens
+code --list-extensions
+
+xampp-windows-x64-8.1.6-0-VS16-installer.exe --mode unattended --launchapps 0
+
+Composer-Setup.6.3.0.exe /VERYSILENT /LOG /CURRENTUSER
---
+++ installers/memo.md
@@ -0,0 +1,8 @@
+# インストーラ
+
+Git-2.37.2.2-64-bit.exe
+VSCodeUserSetup-x64-1.70.0.exe
+xampp-windows-x64-8.1.6-0-VS16-installer.exe
+
+[Composer-Setup.exe](https://getcomposer.org/Composer-Setup.exe)
+Composer-Setup.6.3.0.exe 2022/8/17時点ではこれだった。
